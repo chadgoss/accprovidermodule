b1ad320b9253cb72457c8b1106cda5af18e6d478
Chad Goss <chad.goss@viasat.com>
2021-11-06 10:21:52 -0400

    test

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/chadgoss/accprovidermodule
 
 go 1.17
-
-require github.com/elliotforbes/test-package v1.0.0
--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
     "fmt"
-
-    sample "github.com/elliotforbes/test-package"
 )
 
 func main() {
     fmt.Println("Hello World")
-    sample.MySampleFunc()
 }
 
